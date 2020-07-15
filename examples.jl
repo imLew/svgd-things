8d6026df4bfb9c7a925c4aaca028792052a7d585
imlew <kol@posteo.de>
2020-07-15 21:33:09 +0200

    add unbiased estimator for dKL

--- examples.jl
+++ examples.jl
@@ -51,11 +51,12 @@ function gaussian_1d_mixture(;n_particles=100, step_size=1, repulsion=1, n_iter=
                                 μ₁ = -2, Σ₁ = 1, μ₂ = 2, Σ₂ = 1)
     target = MixtureModel(Normal[ Normal(μ₁, Σ₁), Normal(μ₂, Σ₂) ], [1/3, 2/3])
     p = rand(target, n_particles)
-    tglp(x) = gradp(target, x)
+    grad_logp(x) = gradp(target, x)
     initial_dist = Normal(-10)
     q_0 = rand(initial_dist, (1, n_particles) )
     q = copy(q_0)
-    svgd_fit_with_int(q, tglp, n_iter=n_iter, repulsion=repulsion, step_size=step_size)	
+    q, dKL = svgd_fit_with_int(q, grad_logp, n_iter=n_iter, repulsion=repulsion, step_size=step_size)	
+    return q, q_0, p, dKL
 end
 
 collect_stein_discrepancies(particle_sizes=[10, 50, 100, 200, 300], 
@@ -76,35 +77,28 @@ function plot_discrep(filename, func; n_iter=nothing, title=nothing, label=nothi
     label=label)
 end
 
-for n in [10, 20, 50, 100]
-    if n != 100
-        plot_discrep("collected_data/2d_gaussian_mixture/dKL_"*string(n)*"_particles",
-                     x->mean(eachcol(x)),
-                     # n_iter=100,
-                     title="2D Gaussian Mixture Sampling with "*string(n)*" Particles",
-                     label="Mean of Empirical Stein Discrepancy 100 Runs")
-    else
-        plot_discrep("collected_data/2d_gaussian_mixture/dKL_"*string(n)*"_particles",
-                     x->mean(eachcol(x)),
-                     # n_iter=100,
-                     title="2D Gaussian Mixture Sampling with "*string(n)*" Particles",
-                     label="Mean of Empirical Stein Discrepancy 10 Runs")
-    end
-    Plots.savefig("plots/gaussian_mixture2d_"*string(n)*"_mean.png")
-    pause(5)
-end
+Random.seed!(69)
+Z = gaussian_2d(n_iter=500, step_size=0.5, norm_method="standard")
 
+Random.seed!(69)
+X = gaussian_2d(n_iter=500, step_size=0.5, norm_method="unbiased")
+
+Plots.plot(X[4])
 
 function gaussian_2d(;n_particles=100, step_size=1, repulsion=1, n_iter=500,
                      μ_target=[-2, 8], Σ_target=[9. 0.5; 0.5 1.],
-                     μ_initial = [0, 0], Σ_initial = [1. 0.; 0. 1.])
+                     μ_initial = [0, 0], Σ_initial = [1. 0.; 0. 1.],
+                    norm_method="standard")
     target = MvNormal(μ_target, Σ_target)
     p = rand(target, n_particles)
-    tglp(x) = gradp(target, x)
+    grad_logp(x) = gradp(target, x)
     initial = MvNormal(μ_initial, Σ_initial)
     q_0 = rand(initial, n_particles)
     q = copy(q_0)
-    svgd_fit_with_int(q, tglp, n_iter=n_iter, repulsion=repulsion, step_size=step_size)	
+    q, dKL = svgd_fit_with_int(q, grad_logp, n_iter=n_iter, 
+                               repulsion=repulsion, step_size=step_size,
+                               norm_method=norm_method)
+    return q, q_0, p, dKL
 end
 
 function collect_stein_discrepancies(;particle_sizes, problem_function, dir_name, 
@@ -154,7 +148,7 @@ for n_particles in [10, 20, 50, 100]
             Σ₂ = [1. 0.1; 0.1 7.]  # MvNormal can deal with Int type means but not covariances
             target = MixtureModel(MvNormal[ MvNormal(μ₁, Σ₁), MvNormal(μ₂, Σ₂) ], [0.5, 0.5])
             p = rand(target, n_particles)
-            tglp(x) = gradp(target, x)
+            grad_logp(x) = gradp(target, x)
 
             # initial
             μ = [0, 0]
@@ -163,8 +157,8 @@ for n_particles in [10, 20, 50, 100]
             q_0 = rand(initial, n_particles)
             q = copy(q_0)
 
-            @time q, int_dKL, dKL = svgd_fit_with_int(q, tglp, n_iter=n_iter, repulsion=r, step_size=e)	
-            #= @time q = svgd_fit(q, tglp, n_iter=400, repulsion=r, step_size=e) =#	
+            @time q, int_dKL, dKL = svgd_fit_with_int(q, grad_logp, n_iter=n_iter, repulsion=r, step_size=e)	
+            #= @time q = svgd_fit(q, grad_logp, n_iter=400, repulsion=r, step_size=e) =#	
             Plots.plot(dKL)
             #= plot_svgd_results(q_0, q, p) =#
         end
@@ -187,7 +181,7 @@ begin  # 3d gaussian
     Σ₂ = [1. 0 0; 0 1 0; 0 0 1.]  # MvNormal can deal with Int type means but not covariances
     target = MixtureModel(MvNormal[ MvNormal(μ₁, Σ₁), MvNormal(μ₂, Σ₂) ], [0.5, 0.5])
     p = rand(target, n_particles)
-    tglp(x) = gradp(target, x)
+    grad_logp(x) = gradp(target, x)
 
     # initial
     μ = [-2, -2, -2]
@@ -196,7 +190,7 @@ begin  # 3d gaussian
     q_0 = rand(initial, n_particles)
     q = copy(q_0)
 
-    @time q = svgd_fit(q, tglp, n_iter=400, repulsion=r, step_size=e)	
+    @time q = svgd_fit(q, grad_logp, n_iter=400, repulsion=r, step_size=e)	
 
     plot_svgd_results(q_0, q, p)
 end
@@ -249,9 +243,9 @@ begin  # 2D regression example
 
     q = randn(n_dim+1, n_samples)
     q0 = copy(q)
-    glp(w) = logistic_grad_logp(data, w)
+    grad_logp(w) = logistic_grad_logp(data, w)
 
-    q = svgd_fit(q, glp, n_iter=n_iter, step_size=step_size)
+    q = svgd_fit(q, grad_logp, n_iter=n_iter, step_size=step_size)
 
     traindata = copy(data)
     traindata[:, 1] = log_regr(data[:,2:end], mean(q, dims=2))
