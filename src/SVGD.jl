7d1e32d72c05007bc29462940b9a440f02d80b89
lew <kol@posteo.de>
2020-08-04 20:36:43 +0200

    add RKHS norm

--- src/SVGD.jl
+++ src/SVGD.jl
@@ -1,5 +1,16 @@
 module SVGD
 
+export svgd_fit_with_int
+export median_trick
+export kernelmatrix
+export kernel_grad_matrix
+export empirical_RKHS_norm
+export compute_phi_norm
+export unbiased_stein_discrep
+export stein_discrep_biased
+export svgd_step_with_int
+
+
 using Statistics
 using KernelFunctions
 using LinearAlgebra
@@ -18,7 +29,7 @@ end
 
 grad(f,x,y) = gradient(f,x,y)[1]
 
-function svgd_fit_with_int(q, glp ;n_iter=100, repulsion=1, step_size=1,
+function svgd_fit_with_int(q, grad_logp ;n_iter=100, repulsion=1, step_size=1,
                           norm_method="standard")
     i = 0
     dKL_steps = []
@@ -30,48 +41,56 @@ function svgd_fit_with_int(q, glp ;n_iter=100, repulsion=1, step_size=1,
         @info "Step" i 
         i += 1
         #= kernel.transform.s .= 1/sqrt(median_trick(q)) =#
-        q, dKL = svgd_step_with_int(q, kernel, glp, step_size, repulsion,
-                                   norm_method=norm_method)
+        q, dKL = svgd_step_with_int(q, kernel, grad_logp, step_size, repulsion, norm_method=norm_method)
         push!(dKL_steps, dKL)
         # Plots.display(plot_svgd_results(q_0, q, p, title="$i"))
     end
     return q, dKL_steps
 end
 
-function svgd_step_with_int(X, kernel::KernelFunctions.Kernel, grad_logp, 
+function svgd_step_with_int(q, kernel::KernelFunctions.Kernel, grad_logp, 
                             step_size, repulsion; norm_method="standard")
-    n = size(X)[end]
-    k_mat = kernelmatrix(kernel, X)
-    grad_k = kernel_grad_matrix(kernel, X)
-    glp_mat = hcat( grad_logp.(eachcol(X))... )
-    if n == 1
-        X += step_size/n *  glp_mat * k_mat 
+    n = size(q)[end]
+    k_mat = kernelmatrix(kernel, q)
+    grad_k = kernel_grad_matrix(kernel, q)
+    glp_mat = hcat( grad_logp.(eachcol(q))... )
+    if n == 1  #  no repulsion in case of one particle
+        ϕ = step_size/n * glp_mat * k_mat 
     else
-        X += step_size/n * ( glp_mat * k_mat 
+        ϕ =  step_size/n * ( glp_mat * k_mat 
                     + repulsion * hcat( sum(grad_k, dims=2)... ) 
                    )
     end
-    @time dKL = compute_phi_norm(X, kernel, grad_logp, norm_method=norm_method)
-    return X, dKL
+    q += ϕ
+    @time dKL = compute_phi_norm(q, kernel, grad_logp, norm_method=norm_method, ϕ=ϕ)
+    return q, dKL
 end
 
-function compute_phi_norm(X, kernel, grad_logp; norm_method="standard")
+function compute_phi_norm(q, kernel, grad_logp; norm_method="standard", ϕ=nothing)
     if norm_method == "standard"
-        stein_discrep_biased(X, kernel, grad_logp)
+        stein_discrep_biased(q, kernel, grad_logp)
     elseif norm_method == "unbiased"
-        unbiased_stein_discrep(X, kernel, grad_logp)
-    # elseif method == "RKHS_norm"
+        unbiased_stein_discrep(q, kernel, grad_logp)
+    elseif norm_method == "RKHS_norm"
+        if size(q)[1] == 1
+            empirical_RKHS_norm(kernel, q, ϕ)
+        end
     end
 end
-        
 
-function unbiased_stein_discrep(X, kernel, grad_logp)
-    n = size(X)[end]
+function empirical_RKHS_norm(kernel::Kernel, q, ϕ)
+    k_mat = kernelmatrix(kernel, q)
+    x = ϕ * inv(k_mat) * ϕ'
+    x[1]  # otherwise it's an array of array instead of array of floats
+end
+
+function unbiased_stein_discrep(q, kernel, grad_logp)
+    n = size(q)[end]
     h = 1/kernel.transform.s[1]^2
-    d = size(X)[1]
+    d = size(q)[1]
     dKL = 0
-    for (i, x ) in enumerate(eachcol(X))
-        for (j, y ) in enumerate(eachcol(X))
+    for (i, x ) in enumerate(eachcol(q))
+        for (j, y ) in enumerate(eachcol(q))
             if i != j
                 dKL += kernel(x,y) * grad_logp(x)' * grad_logp(y)
                 dKL += gradient(x->kernel(x,y), x)[1]'*grad_logp(y)
@@ -83,13 +102,13 @@ function unbiased_stein_discrep(X, kernel, grad_logp)
     dKL /= n*(n-1)
 end
 
-function stein_discrep_biased(X, kernel, grad_logp) 
-    n = size(X)[end]
+function stein_discrep_biased(q, kernel, grad_logp) 
+    n = size(q)[end]
     h = 1/kernel.transform.s[1]^2
-    d = size(X)[1]
+    d = size(q)[1]
     dKL = 0
-    for x  in eachcol(X)
-        for y  in eachcol(X)
+    for x  in eachcol(q)
+        for y  in eachcol(q)
             dKL += kernel(x,y) * grad_logp(x)' * grad_logp(y)
             dKL += gradient(x->kernel(x,y), x)[1]'*grad_logp(y)
             dKL += gradient(y->kernel(x,y), y)[1]'*grad_logp(x)
@@ -99,61 +118,23 @@ function stein_discrep_biased(X, kernel, grad_logp)
     dKL /= n^2
 end
 
-# function stein_discrep_biased(X, kernel, grad_logp)
-#     n = size(X)[end]
-#     h = 1/kernel.transform.s[1]^2
-#     k_mat = kernelmatrix(kernel, X)
-#     grad_k = kernel_grad_matrix(kernel, X)
-#     glp_mat = hcat( grad_logp.(eachcol(X))... )
-#     dKL = n^2\sum( k_mat .* ( glp_mat' * glp_mat + 2*size(X)[1]/h * ones(n,n)
-#                              - 4/h^2 * Distances.pairwise(SqEuclidean(), X, dims=2)
-#                    )
-#              )
-#     s = 0
-#     for x in eachcol(X)
-#         for y in eachcol(X)
-#             s += gradient(x->kernel(x,y), x)[1]'*grad_logp(y)
-#             s += gradient(y->kernel(x,y), y)[1]'*grad_logp(x)
-#         end
-#     end
-#     dKL += s/n^2
-# end
-
-# function svgd_step(X, kernel::KernelFunctions.Kernel, grad_logp, step_size, repulsion)
-#     n = size(X)[end]
-#     k_mat = kernelmatrix(kernel, X)
-#     grad_k = kernel_grad_matrix(kernel, X)
-#     glp_mat = ghcat( grad_logp.(eachcol(X))... )
-#     X += step_size/n * ( glp_mat * k_mat 
-#                 + repulsion * hcat( sum(grad_k, dims=2)... ) 
-#                )
-# end
-
-# function svgd_fit(q, glp ;n_iter=100, repulsion=1, step_size=1)
-#     i = 0
-#     kernel = TransformedKernel( SqExponentialKernel(), ScaleTransform( 1/sqrt(median_trick(q))))
-#     while i < n_iter
-#         i += 1
-#         kernel.transform.s .= median_trick(q)
-#         q = svgd_step(q, kernel, glp, step_size, repulsion)
-#     end
-#     return q
-# end
-
-#= function grad_logp_matrix(grad_logp, X) =#
-#=     hcat( grad_logp.(eachcol(X))... ) =#
+#= function grad_logp_matrix(grad_logp, q) =#
+#=     hcat( grad_logp.(eachcol(q))... ) =#
 #= end =#
 
 #= function kernel_gradient(kernel::KernelFunctions.Kernel, x, y) =#
 #=     gradient(x->kernel(x,y), x) =#
 #= end =#
 
+function kernelmatrix(kernel, q)
+    mapslices( x-> kernel.([x], eachcol(q)), q, dims=1 )
+end
 
-function kernel_grad_matrix(kernel::KernelFunctions.Kernel, X)
-    if size(X)[end] == 1
+function kernel_grad_matrix(kernel::KernelFunctions.Kernel, q)
+    if size(q)[end] == 1
         return 0
     end
-	mapslices(x -> grad.(kernel, [x], eachcol(X)), X, dims = 1)
+	mapslices(x -> grad.(kernel, [x], eachcol(q)), q, dims = 1)
 end
 
 end # module
