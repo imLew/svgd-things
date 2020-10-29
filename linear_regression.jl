9dd70a89e5247a4599f1b9bb368c86a76585fb12
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-10-29 17:19:08 +0100

    Added MAP_start

--- linear_regression.jl
+++ linear_regression.jl
@@ -4,8 +4,9 @@ using BSON
 using Distributions
 using DataFrames
 using LinearAlgebra
+using Optim
 
-using SVGD
+using SVGD #include(joinpath(@__DIR__, "src", "SVGD.jl"))
 
 global DIRNAME = "linear_regression"
 
@@ -60,23 +61,33 @@ function grad_log_likelihood(D::RegressionData, model::RegressionModel)
     model.β * sum( ( D.t .- y(model).(D.x) ) .* model.ϕ.(D.x) )
 end
 
-function fit_linear_regression(problem_params, alg_params, D) 
-    # we use the prior as the initial distribution of the particles
-    initial_dist = MvNormal(problem_params[:μ_prior],
-                            problem_params[:Σ_prior])
-
-    q = rand(initial_dist, alg_params[:n_particles])
+function fit_linear_regression(problem_params, alg_params, D; MAP_start = true)
+    function logp(w)
+        model = RegressionModel(problem_params[:ϕ], w, problem_params[:true_β])
+        log_likelihood(D, model) + logpdf(MvNormal(problem_params[:μ_prior], problem_params[:Σ_prior]), w)
+    end
     function grad_logp(w) 
         model = RegressionModel(problem_params[:ϕ], w, problem_params[:true_β])
         (grad_log_likelihood(D, model) 
             .- inv(problem_params[:Σ_prior]) * (w-problem_params[:μ_prior])
         )
     end
+    grad_logp!(g, w) = g .= grad_logp(w)
+    # we use the prior as the initial distribution of the particles
+    global μ_prior = if MAP_start
+        Optim.maximizer(Optim.maximize(logp, grad_logp!, problem_params[:μ_prior], LBFGS()))
+    else
+        problem_params[:μ_prior]
+    end
 
-    q, hist = svgd_fit(q, grad_logp; alg_params...)
+
+    initial_dist = MvNormal(μ_prior, problem_params[:Σ_prior])
+    q = rand(initial_dist, alg_params[:n_particles])
+
+
+    q, hist = SVGD.svgd_fit(q, grad_logp; alg_params...)
     return initial_dist, q, hist
 end
-
 # the other numerical_expectation function applies f to each element instead
 # of each col :/
 function num_expectation(d::Distribution, f; n_samples=10000)
@@ -146,13 +157,13 @@ function plot_results(plt, q, problem_params)
     return plt
 end
 
-### Experiments - linear regression on 3 basis functions
+## Experiments - linear regression on 3 basis functions
 
 alg_params = Dict(
     :step_size => 0.0001,
     :n_iter => 1000,
-    :n_particles => 50,
-    :kernel_width => "median_trick"
+    :n_particles => 10,
+    :kernel_width => "median_trick",
 )
 
 
@@ -164,7 +175,7 @@ problem_params = Dict(
     :true_β => 2,
     :ϕ => x -> [x, x^2, x^4, x^3],
     :μ_prior => zeros(4),
-    :Σ_prior => I(4),
+    :Σ_prior => 1.0I(4),
 )
 
 n_runs = 1
@@ -182,24 +193,24 @@ D = generate_samples(model=true_model,
                     )
 
 initial_dist, q, hist = fit_linear_regression(problem_params, alg_params, D)
-        H₀ = Distributions.entropy(initial_dist)
-        EV = ( num_expectation( 
+H₀ = Distributions.entropy(initial_dist)
+EV = ( num_expectation( 
                     initial_dist, 
                     w -> log_likelihood(D, 
                             RegressionModel(problem_params[:ϕ], w, 
                                             problem_params[:true_β])) 
                )
-               + expectation_V(initial_dist, initial_dist) 
+               + SVGD.expectation_V(initial_dist, initial_dist) 
                + 0.5 * log( det(2π * problem_params[:Σ_prior]) )
               )
-        est_logZ = estimate_logZ(H₀, EV,
+est_logZ = SVGD.estimate_logZ(H₀, EV,
                             alg_params[:step_size] * sum( get(hist,:dKL_rkhs)[2] ) 
                                  )
 
-norm_plot = plot(hist[:ϕ_norm])
+norm_plot = plot(hist[:ϕ_norm], title = "φ norm", yaxis = :log)
 int_plot = plot(
-    estimate_logZ.([H₀], [EV], alg_params[:step_size] * cumsum( get(hist,:dKL_rkhs)[2])
-    )
+    SVGD.estimate_logZ.([H₀], [EV], alg_params[:step_size] * cumsum( get(hist, :dKL_rkhs)[2]))
+    ,title = "log Z", label = "",
 )
 fit_plot = plot_results(plot(size=(300,250)), q, problem_params)
 plot(fit_plot, norm_plot, int_plot, layout=@layout [f; n i])
