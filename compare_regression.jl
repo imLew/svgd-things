ec4c9ab77e360ccda0d05668d7a2b05194d518cf
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-11-02 16:13:08 +0100

    Some fixes

--- compare_regression.jl
+++ compare_regression.jl
@@ -2,16 +2,16 @@ include("linear_regression.jl")
 include("src/therm_int.jl")
 
 # set up
-n_dim = 4
+n_dim = 3
 problem_params = Dict(
     :n_samples => 20,
     :sample_range => [-3, 3],
-    :true_ϕ => x -> [1, x, x^2],
+    :true_ϕ => x -> [one(x), x, x^2],
     :true_w => [2, -1, 0.2],
     :true_β => 2,
     :ϕ => x -> [1, x, x^2],
     :μ_prior => zeros(n_dim),
-    :Σ_prior => 1.0I(n_dim),
+    :Σ_prior => 0.05I(n_dim),
     :MAP_start => true,
 )
 
@@ -20,7 +20,9 @@ true_model = RegressionModel(problem_params[:true_ϕ],
                              problem_params[:true_β])
 D = generate_samples(model=true_model, n_samples=problem_params[:n_samples],
                      sample_range=problem_params[:sample_range])
-
+scale = sum(extrema(D.t))
+problem_params[:true_w] ./= scale
+D.t ./= scale
 ###############################################################################
 ## ThermoIntegration
 ## alg params 
@@ -54,12 +56,24 @@ alg_params = Dict(
 )
 initial_dist, q, hist = fit_linear_regression(problem_params, alg_params, D)
 H₀ = Distributions.entropy(initial_dist)
-EV = ( num_expectation( 
-            initial_dist, 
-            w -> log_likelihood(D, RegressionModel(problem_params[:ϕ], w, 
-                                                   problem_params[:true_β])) )
+
+function true_gauss_expectation(d::MvNormal, m::RegressionModel, D::RegressionData)
+    X = reduce(hcat, m.ϕ.(D.x))
+    @show size(X)
+    0.5 * m.β * (tr((mean(d) * mean(d)' + cov(d)) * X * X')
+        - 2 * D.t' * X' * mean(d)
+        + D.t' * D.t
+        + length(D) * log(m.β / 2π))
+end
+
+EV = ( true_gauss_expectation(initial_dist,  RegressionModel(problem_params[:ϕ], mean(initial_dist), 
+problem_params[:true_β]), D)
+        # num_expectation( 
+            # initial_dist, 
+            # w -> log_likelihood(D, RegressionModel(problem_params[:ϕ], w, 
+                                                #    problem_params[:true_β])) )
        + SVGD.expectation_V(initial_dist, initial_dist) 
-       + 0.5 * log( det(2π * problem_params[:Σ_prior]) )
+       + 0.5 * logdet(2π * problem_params[:Σ_prior])
       )
 est_logZ_rkhs = SVGD.estimate_logZ(H₀, EV,
                 alg_params[:step_size] * sum( get(hist,:dKL_rkhs)[2] ) 
@@ -75,7 +89,7 @@ est_logZ_stein_discrep = SVGD.estimate_logZ(H₀, EV,
 ## compare results
 
 true_logZ = regression_logZ(problem_params[:Σ_prior], problem_params[:true_β], 
-                            problem_params[:true_ϕ], D)
+                            problem_params[:true_ϕ], D.x)
 
 @info "Value comparison" true_logZ tlZ
 @info "Value comparison" true_logZ therm_logZ est_logZ_rkhs est_logZ_stein_discrep est_logZ_unbiased
